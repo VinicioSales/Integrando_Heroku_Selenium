4ee693ef93b21738566c8dc57b88ca78144157f5
VinicioSales <vinicio.santtana@gmail.com>
2022-07-07 15:51:14 -0300

    Att

--- API.py
+++ API.py
@@ -4,11 +4,17 @@ from selenium.webdriver.chrome.service import Service
 import time
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import os
 
+op = webdriver.ChromeOptions()
+op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+op.add_argument("--headless")
+op.add_argument("--no-sandbox")
+op.add_argument("--disable-dev-sh-usage")
 
-
-servico = Service(ChromeDriverManager().install())
-navegador = webdriver.Chrome(service=servico)
+#servico = Service(ChromeDriverManager().install())
+#navegador = webdriver.Chrome(service=servico)
+navegador = webdriver.Chrome(executable_path= os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op)
 
 navegador.get("https://www.google.com/")
 
